2ccb5a2567cbf2623ef351e75b5919ac7b47eddb
= <=>
2020-12-11 00:03:08 +0700

    Create CreateCourse.java and activity_create_course.xml

---
+++ app/src/main/java/Activity/CreateCourse.java
@@ -0,0 +1,14 @@
+package Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.tutorial_v1.R;
+import android.os.Bundle;
+
+public class CreateCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_course);
+    }
+}
